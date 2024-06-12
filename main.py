94cf6b59a21526595fd85436b00a6f73e4aaced5
Zingo21 <kamparassignar@gmail.com>
2024-06-12 02:42:05 +0200

    Add joined and slap commands

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from discord.ext import commands
 logger = settings.logging.getLogger("bot")
 
 
+class Slapper(commands.Converter):
+    use_nicknames = bool
+
+    def __init__(self, *, use_nicknames) -> None:
+        self.use_nicknames = use_nicknames
+
+    async def convert(self, ctx, argument):
+        someone = random.choice(ctx.guild.members)
+        nickname = ctx.author
+        if self.use_nicknames:
+            nickname = ctx.author.nick
+
+        return f"{nickname} slaps {someone} with {argument}"
+
 def run():
     intents = discord.Intents.default()
     intents.message_content = True
@@ -45,6 +59,14 @@ def run():
     @bot.command()
     async def add(ctx, one : int, two : int):
         await ctx.send(one, two)
+
+    @bot.command()
+    async def joined(ctx, who : discord.Member):
+        await ctx.send(who.joined_at)
+
+    @bot.command()
+    async def slap(ctx, reason : Slapper(use_nicknames=True)):
+        await ctx.send(reason)
 
     @bot.command()
     async def say3(ctx, what="WHAT?", why = "WHY?"):
